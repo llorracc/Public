dc24953ce9eaaddc824b779c72b6879ba6857182
llorracc <git@llorracc.org>
2020-04-06 13:14:07 -0400

    Update akmwwInequality

---
+++ Discuss/2017-04-NBER-Macro-Annual/akmwwInequality/Figures/makeLinks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+#ln -fs /Volumes/Sync/drive.google.com/@llorracc.org/ccarroll/ccarroll/Work/Discuss/2015-05-18_FedNY_DiMaggio_Kermani/LaTeX/cfpbpresentation/Figures/Retail-Sales-Collapse.jpg .
+#ln -fs /Volumes/Sync/drive.google.com/@llorracc.org/ccarroll/ccarroll/Work/Discuss/2015-05-18_FedNY_DiMaggio_Kermani/LaTeX/cfpbpresentation/Figures/Retail-Sales-Collapse.PNG .
+
+ln -fs /Volumes/Data/Work/BufferStock/Macro/cstwMPC/Latest/Figures/CFuncDistSevenPointPermAndHistNetWorthPlotFedQuarterly.pdf
+ln -fs /Volumes/Data/Work/BufferStock/Macro/cstwMPC/Latest/Figures/CFuncDistSevenPointPermAndHistNetWorthPlotFedQuarterly.PNG .
